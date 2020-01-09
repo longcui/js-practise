00de13dfa91d5b6e5dcb86e0843f505ca873fe0a
longcui <iamlongcui@gmail.com>
2020-01-09 12:19:42 +0100

    init

---
+++ promise.test.js
@@ -0,0 +1,14 @@
+const promiseInst = new Promise(function (resolve, reject) {
+    const x = "geeksforgeeks";
+    const y = "geeksforgeeks"
+    if (x === y) {
+        resolve();
+    } else {
+        reject();
+    }
+});
+
+
+test("", function() {
+
+});
